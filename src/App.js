5613905d61e510ff1e95fc26a747a5882b02988e
Marco Civello <marco.civello@gmail.com>
2021-06-22 14:00:52 +0200

    feat: added API call logic

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-  return <div className='App'>Jagaad Challenge</div>;
+  // API data logic
+  const [activities, setActivities] = useState([]);
+
+  useEffect(() => {
+    fetch('https://api.musement.com/api/v3/venues/164/activities?limit=60&offset=0')
+      .then((response) => response.json())
+      .then((data) => {
+        setActivities(data);
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  }, []);
+
+  console.log(activities);
+
+  return <div className='App'></div>;
 }
 
 export default App;

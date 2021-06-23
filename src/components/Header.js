f192250b2ca49ce41c67cbd97ceadafe84f1180b
Marco Civello <marco.civello@gmail.com>
2021-06-23 16:14:19 +0200

    feat: added openmodal onclick to header button

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './Header.css';
 
-function Header() {
+function Header({ onWishlistClick }) {
   return (
     <header className='header__container'>
       <Link to='/' className='header__text'>
         JAG<span>AA</span>D TEST
       </Link>
       <div className='header__links'>
-        <button type='button' className='header__btn'>
+        <button onClick={onWishlistClick} type='button' className='header__btn'>
           <i className='fas fa-star'></i>(99)
         </button>
       </div>

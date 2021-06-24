1a4b5a37bb50aa8be65b96cf4043833eb992fb6b
Marco Civello <marco.civello@gmail.com>
2021-06-24 18:04:25 +0200

    fix: added wishlist.length to header button
    show number of items

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './Header.css';
 
-function Header({ onWishlistClick }) {
+function Header({ wishlist, onWishlistClick }) {
   return (
     <header className='header__container'>
       <Link to='/' className='header__text'>
@@ -12,7 +12,7 @@ function Header({ onWishlistClick }) {
       </Link>
       <div className='header__links'>
         <button onClick={onWishlistClick} type='button' className='header__btn'>
-          <i className='fas fa-star'></i>(99)
+          <i className='fas fa-star'></i>({wishlist.length})
         </button>
       </div>
     </header>

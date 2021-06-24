376d0005e0802e4cfa18101157098f27ecfcabae
Marco Civello <marco.civello@gmail.com>
2021-06-24 18:06:54 +0200

    fix: added wishlist.length to modal and clear all
    show number of items and delete all items in modal

--- src/components/ModalSidebar.js
+++ src/components/ModalSidebar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './ModalSidebar.css';
 
-function ModalSidebar({ isModalOpen, closeModal, children }) {
+function ModalSidebar({ isModalOpen, closeModal, clearWishlist, wishlist, children }) {
   return (
     <div className={`ModalSidebar ${isModalOpen ? `is-open` : ''}`}>
       <div className='ModalSidebar__overlay' onClick={closeModal}></div>
@@ -10,10 +10,12 @@ function ModalSidebar({ isModalOpen, closeModal, children }) {
           <button className='ModalSidebar__close' onClick={closeModal}>
             X
           </button>
-          <h2 className='ModalSidebar__title'>WISHLIST (99)</h2>
-          <button type='button' className='ModalSidebar__btn'>
-            Clear All
-          </button>
+          <h2 className='ModalSidebar__title'>WISHLIST ({wishlist.length})</h2>
+          {wishlist.length > 0 && (
+            <button onClick={() => clearWishlist()} type='button' className='ModalSidebar__btn'>
+              Clear All
+            </button>
+          )}
         </header>
         {children}
       </div>
